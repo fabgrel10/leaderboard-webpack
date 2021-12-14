21eb057607be5f7494be119687a668d048104d45
Fabricio Garcia <fabgrel10@gmail.com>
2021-12-13 22:20:42 -0600

    ✨ Add basic functionality
    * app.js:
     - Create event listeners for initial load, refresh button and form
    
    * helpers.js
     - Create funtions to add score to the localStorage, creal the HTML, and
       render the scores in the screen.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
 import './scss/app.scss';
+import { addScoreToLocalStorage, renderScoreboard } from './helpers/helpers';
+
+const form = document.getElementById('add-score-form');
+const name = document.getElementById('name');
+const score = document.getElementById('score');
+const refreshScoreboard = document.getElementById('refresh-scoreboard');
+let scores = [];
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const newScore = {
+    name: name.value,
+    score: score.value,
+  };
+
+  scores.push(newScore);
+  addScoreToLocalStorage(scores);
+  form.reset();
+  renderScoreboard(scores);
+});
+
+refreshScoreboard.addEventListener('click', () => {
+  scores = localStorage.setItem('scores', []);
+  renderScoreboard(scores);
+});
+
+window.addEventListener('load', () => {
+  scores = JSON.parse(localStorage.getItem('scores')) || [];
+  renderScoreboard(scores);
+})
